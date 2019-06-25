cec1f85b13666a158ae6d0d623ffcd7c2872309c
chuiizeet <chuy.guerra19@outlook.es>
2019-06-25 16:10:49 -0500

    Search ui

--- InstaCopy/InstaCopy/Controller/Login/SignUpVC.swift
+++ InstaCopy/InstaCopy/Controller/Login/SignUpVC.swift
@@ -130,7 +130,7 @@ class SignUpVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
         guard let email = emailTextField.text else { return }
         guard let password = passwordTextField.text else { return }
         guard let fullName = fullNameTextField.text else { return }
-        guard let username = usernameTextField.text else { return }
+        guard let username = usernameTextField.text?.lowercased() else { return }
         
         Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
             // Handle error
@@ -178,6 +178,15 @@ class SignUpVC: UIViewController, UIImagePickerControllerDelegate, UINavigationC
                     Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (error, ref) in
                         
                         print("Save user data to database")
+                        
+                        guard let mainTabVC = UIApplication.shared.keyWindow?.rootViewController as? MainTabVC else { return }
+                        
+                        // Config vc in maintabvc
+                        mainTabVC.configureViewControllers()
+                        
+                        // Dismiss login controller
+                        self.dismiss(animated: true, completion: nil)
+                        
                     })
                     
                 })
---
+++ InstaCopy/InstaCopy/View/SearchUserCell.swift
@@ -0,0 +1,56 @@
+//
+//  SearchUserCell.swift
+//  InstaCopy
+//
+//  Created by imac on 6/25/19.
+//  Copyright © 2019 JeguLabs. All rights reserved.
+//
+
+import UIKit
+
+class SearchUserCell: UITableViewCell {
+    
+    // MARK - Properties
+    
+    let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.backgroundColor = .red
+        return iv
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        // Add profile image view
+        addSubview(profileImageView)
+        profileImageView.anchor(top: nil, left: leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 48, height: 48)
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.layer.cornerRadius = 48 / 2
+        profileImageView.clipsToBounds = true
+        
+        self.textLabel?.text = "Username"
+        
+        self.detailTextLabel?.text = "Full name"
+        
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel?.frame = CGRect(x: 68, y: (textLabel?.frame.origin.y)! - 2, width: (textLabel?.frame.width)!, height: (textLabel?.frame.height)!)
+        
+        textLabel?.font = UIFont.boldSystemFont(ofSize: 12)
+        
+        detailTextLabel?.frame = CGRect(x: 68, y: (detailTextLabel?.frame.origin.y)! - 2, width: (detailTextLabel?.frame.width)!, height: (detailTextLabel?.frame.height)!)
+        detailTextLabel?.textColor = .lightGray
+        detailTextLabel?.font = UIFont.systemFont(ofSize: 12)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
